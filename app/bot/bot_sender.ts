255d4dcd68a094fe833bb8bc52f29aba070c34e8
infy <user.mg1216@gmail.com>
2026-04-23 12:12:43 +0300

    feat: notification toggle functionality

--- app/bot/bot_sender.ts
+++ app/bot/bot_sender.ts
@@ -1,4 +1,4 @@
-import { getChannelFollowers } from "../database/db";
+import { getChannelFollowers, getSettingsState } from "../database/db";
 import logger from "../logger";
 import { botInstance as bot } from "./bot";
 
@@ -11,17 +11,20 @@ export async function sendStreamOnlineNotificationToUsers(
 ) {
   const followers = getChannelFollowers.all(channel_id);
   for (const follower of followers) {
-    await bot.api.sendMessage(
-      follower.user_id,
-      //@ts-ignore
-      `<b>${channel_name}</b> ведет прямую трансляцию.\n${data.title}\n<i>${data.game_name}</i>\n\n<a href="https://twitch.tv/${channel_name}">Twitch</a>`,
-      { parse_mode: "HTML" },
-    );
-    log.info("message sent", {
-      user_id: follower.user_id,
-      //@ts-ignore
-      text: `<b>${channel_name}</b> ведет прямую трансляцию.\n${data.title}\n<i>${data.game_name}</i>\n\n<a href="https://twitch.tv/${channel_name}">Twitch</a>`,
-    });
+    const userSettings = getSettingsState.get(follower.user_id);
+    if (userSettings?.online_notification === 1) {
+      await bot.api.sendMessage(
+        follower.user_id,
+        //@ts-ignore
+        `<b>${channel_name}</b> ведет прямую трансляцию.\n${data.title}\n<i>${data.game_name}</i>\n\n<a href="https://twitch.tv/${channel_name}">Twitch</a>`,
+        { parse_mode: "HTML" },
+      );
+      log.info("message sent", {
+        user_id: follower.user_id,
+        //@ts-ignore
+        text: `<b>${channel_name}</b> ведет прямую трансляцию.\n${data.title}\n<i>${data.game_name}</i>\n\n<a href="https://twitch.tv/${channel_name}">Twitch</a>`,
+      });
+    }
   }
 }
 
@@ -31,15 +34,18 @@ export async function sendStreamOfflineNotificationToUsers(
 ) {
   const followers = getChannelFollowers.all(channel_id);
   for (const follower of followers) {
-    await bot.api.sendMessage(
-      follower.user_id,
-      `<b>${channel_name}</b> завершил прямую трансляцию.`,
-      { parse_mode: "HTML" },
-    );
-    log.info("message sent", {
-      user_id: follower.user_id,
-      //@ts-ignore
-      text: `<b>${channel_name}</b> завершил прямую трансляцию.`,
-    });
+    const userSettings = getSettingsState.get(follower.user_id);
+    if (userSettings?.offline_notification === 1) {
+      await bot.api.sendMessage(
+        follower.user_id,
+        `<b>${channel_name}</b> завершил прямую трансляцию.`,
+        { parse_mode: "HTML" },
+      );
+      log.info("message sent", {
+        user_id: follower.user_id,
+        //@ts-ignore
+        text: `<b>${channel_name}</b> завершил прямую трансляцию.`,
+      });
+    }
   }
 }
