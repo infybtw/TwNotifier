b1919fee585ed9e4e7a17916f35724acdbe1faaf
infy <user.mg1216@gmail.com>
2026-07-05 15:02:08 +0300

    fix: drizzle orm migration errors

--- app/drizzle.config.ts
+++ app/drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./app/database/schema.ts",
+  schema: "./database/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
--- app/migrate.ts
+++ app/migrate.ts
@@ -5,7 +5,7 @@ const db = drizzle(process.env.DATABASE_URL!);
 
 export async function migrateDB() {
   try {
-    await migrate(db, { migrationsFolder: "./drizzle" });
+    await migrate(db, { migrationsFolder: "./app/drizzle" });
     console.log("Migrations applied successfully");
   } catch (err) {
     console.error("Migration failed:", err);
