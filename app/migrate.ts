bd2afca239f943bb890cb86606e50a7462588089
infy <user.mg1216@gmail.com>
2026-07-07 19:31:39 +0300

    fix: bun run dev - use SQL instance for drizzle and correct env-file path

--- app/migrate.ts
+++ app/migrate.ts
@@ -1,7 +1,9 @@
+import { SQL } from "bun";
 import { migrate } from "drizzle-orm/bun-sql/migrator";
 import { drizzle } from "drizzle-orm/bun-sql";
 
-const db = drizzle(process.env.DATABASE_URL!);
+const sqlConnect = new SQL(process.env.DATABASE_URL!)
+const db = drizzle(sqlConnect);
 
 export async function migrateDB() {
   try {
